5819d06aef89013fe8d7599718e7139feb8d6449
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-05-09 12:26:28 -0600

    MVP complete

---
+++ database.js
@@ -0,0 +1,45 @@
+let users = [
+    { id: "1", name: "Jane Doe", bio: "Not Tarzan's Wife, another Jane" },
+    { id: "2", name: "Frodo Baggins", bio: "The Ringbearer" },
+    { id: "3", name: "Bilbo Baggins", bio: "Frodo's uncle" },
+]
+
+function getUsers() {
+    return users;
+}
+
+function getUserById(id) {
+    return users.find(u => u.id === id)
+}
+
+function createUser(data) {
+    const payload = {
+        id: String(users.length + 1),
+        ...data,
+    }
+
+    users.push(payload)
+    return payload
+}
+
+function updateUser(id, info) {
+    const index = users.findIndex(u => u.id === id)
+    users[index] = {
+        ...users[index],
+        ...info,
+    }
+
+    return users[index]
+}
+
+function deleteUser(id) {
+    users = users.filter(u => u.id != id)
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+}
